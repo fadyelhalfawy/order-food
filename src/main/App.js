32124d6582949528ea991ba48ec76128a5455679
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-13 17:59:21 +0200

    Add Cart component to App.js.

--- src/main/App.js
+++ src/main/App.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from "react";
 import Header from "../components/header/Header";
 import Meals from "../components/meals/Meals";
+import Cart from "../components/cart/Cart";
 
 const App = () => {
         return(
             <Fragment>
+                <Cart />
                 <Header />
 
                 <main>
