bb0c893fdef175e5af3c4b144481ac89e97affeb
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-13 18:19:01 +0200

    Update App.js by adding useState to show and hide cartForm.

--- src/main/App.js
+++ src/main/App.js
@@ -1,13 +1,20 @@
-import React, {Fragment} from "react";
+import React, {useState, Fragment} from "react";
 import Header from "../components/header/Header";
 import Meals from "../components/meals/Meals";
 import Cart from "../components/cart/Cart";
 
 const App = () => {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const showCartHandler = () => setCartIsShown(true);
+
+    const hideCartHandler = () => setCartIsShown(false);
+
         return(
             <Fragment>
-                <Cart />
-                <Header />
+                {cartIsShown && <Cart onClose={hideCartHandler}/>}
+
+                <Header onShow={showCartHandler}/>
 
                 <main>
                     <Meals />
