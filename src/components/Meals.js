4525ba68c43f723b6dafc9a44ed1e3661a25d426
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-13 09:43:48 +0200

    Add Meals component.

---
+++ src/components/Meals.js
@@ -0,0 +1,15 @@
+import {Fragment} from "react";
+import MealsSummary from "./MealsSummary";
+import DummyMeals from "./services/DummyMeals";
+
+const Meals = () => {
+    return(
+    <Fragment>
+        <MealsSummary />
+
+        <DummyMeals />
+    </Fragment>
+    )
+}
+
+export default Meals;
