9764485a97ad641685e10511d81daf070811d93e
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-15 13:35:32 +0200

    Update HeaderCartButton.js by adding map function to get the amount from array of objects then reduce it to get the sum and render it dynamically.

--- src/components/header/HeaderCartButton.js
+++ src/components/header/HeaderCartButton.js
@@ -3,12 +3,15 @@ import CartIcon from "../cart/CartIcon";
 import classes from "../modules/HeaderCartButton.module.css";
 import cartContext from "../cart/cart-context";
 
+const getAmount = item => item.amount;
+
 const HeaderCartButton = ({ onShow }) => {
     const storedCartContext = useContext(cartContext);
-
-    const numberOfCartItems = storedCartContext.items.reduce((a, b) => a + b, 0);
+    const itemAmount = storedCartContext.items.map(getAmount);
+    const numberOfCartItems = itemAmount.reduce((a, b) => a + b, 0);
 
     return (
+
     <button className={classes.button} onClick={onShow}>
 
       <span className={classes.icon}>
@@ -25,6 +28,6 @@ const HeaderCartButton = ({ onShow }) => {
 
     </button>
     );
-}
+};
 
 export default HeaderCartButton;
