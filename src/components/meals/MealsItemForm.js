38496bb4f8f14e1e5b8fe5845e33c9f500b442d8
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-15 13:33:11 +0200

    Update MealsItemForm.js

--- src/components/meals/MealsItemForm.js
+++ src/components/meals/MealsItemForm.js
@@ -1,15 +1,35 @@
+import {useRef, useState} from "react";
 import classes from "../modules/MealItemForm.module.css"
 import InputForm from "../forms/InputForm";
 
-const MealsItemForm = ({ id, ...rest }) => {
+const MealsItemForm = ({ id, onAddToCart, ...rest }) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        onAddToCart(enteredAmountNumber);
+    };
+
     return(
         <form className={classes.form}>
             <InputForm
                 id={id}
+                ref={amountInputRef}
                 { ...rest }
             />
 
-            <button>+ Add</button>
+            <button onClick={submitHandler}>+ Add</button>
+
+            {!amountIsValid && <p>Please enter a valid amount from 1 to 5</p>}
         </form>
     );
 }
