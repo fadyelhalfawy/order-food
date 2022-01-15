63e2b8f50bd6659db529cd96065dda6db65fc90f
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-15 15:03:02 +0200

    Update HeaderCartButton.js by adding useEffect hooks to add animation.

--- src/components/header/HeaderCartButton.js
+++ src/components/header/HeaderCartButton.js
@@ -1,14 +1,33 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartIcon from "../cart/CartIcon";
-import classes from "../modules/HeaderCartButton.module.css";
 import cartContext from "../cart/cart-context";
+import classes from "../modules/HeaderCartButton.module.css";
 
 const getAmount = item => item.amount;
 
 const HeaderCartButton = ({ onShow }) => {
-    const storedCartContext = useContext(cartContext);
-    const itemAmount = storedCartContext.items.map(getAmount);
+    const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
+    const { items } = useContext(cartContext);
+
+    const itemAmount = items.map(getAmount);
     const numberOfCartItems = itemAmount.reduce((a, b) => a + b, 0);
+
+    const btnClasses = `${classes.badge} ${ btnIsHighLighted ? classes.bump : ""}`;
+
+    useEffect(() => {
+        if (items.length === 0) return;
+
+        setBtnIsHighLighted(true);
+
+        const timer = setTimeout( () => {
+            setBtnIsHighLighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
 
     return (
 
@@ -22,7 +41,7 @@ const HeaderCartButton = ({ onShow }) => {
           Your Cart
       </span>
 
-      <span className={classes.badge}>
+      <span className={btnClasses}>
           {numberOfCartItems}
       </span>
 
