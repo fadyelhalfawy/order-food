e58bd219a8bedbd873fb08a57b0027d70ca4b92b
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-15 13:33:54 +0200

    Update CartProvider.js by adding cartReducer function.

--- src/components/cart/CartProvider.js
+++ src/components/cart/CartProvider.js
@@ -1,14 +1,37 @@
+import {useReducer} from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === "Add") {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        return {
+          items: updatedItems,
+          totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
 const CartProvider = props => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const addItemHandler = item => {};
+    const addItemHandler = item => {
+        dispatchCartAction({type: "Add", item: item});
+    };
 
-    const removeItemHandler = id => {};
+    const removeItemHandler = id => {
+        dispatchCartAction({type: "Remove", id: id});
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler
     };
