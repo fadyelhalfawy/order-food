bbf09e9beb38447a042b11aa00cbee25bdf2d0a7
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-15 14:21:02 +0200

    Update Cart.js to make it dynamically to view all meals which are selected by the user and calling CartItem component and check if no items order button will not showen.

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,28 @@
 import classes from "../modules/Cart.module.css";
 import Modal from "../UI/Modal";
+import {useContext} from "react";
+import cartContext from "./cart-context";
+import CartItem from "./CartItem";
 
 const Cart = ({ onClose }) => {
+    const cartConte = useContext(cartContext);
+
+    const hasItems = cartConte.items.length > 0;
+    const totalAmount = `$${cartConte.totalAmount.toFixed(2)}`;
+
+    const cartItemRemove = id => {};
+    const cartItemAdd = item => {};
+
     const cartItems = (
         <ul className={classes['cart-items']}>
-            { [{id: 'c1', name: 'Sushi', amount: '2', price: '12.99'}].map(item => (
-                <li key={item.id}>{item.name}</li>
+            { cartConte.items.map(item => (
+                <CartItem key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onAdd={cartItemAdd.bind(null, item)}
+                    onRemove={cartItemRemove.bind(null, item.id)}
+                />
             )) }
         </ul>
     );
@@ -15,12 +32,12 @@ const Cart = ({ onClose }) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
 
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={onClose}>Close</button>
-                <button className={classes.button}>Order</button>
+                { hasItems && <button className={classes.button}>Order</button> }
             </div>
         </Modal>
     );
