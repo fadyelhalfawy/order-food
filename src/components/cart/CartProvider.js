461cfde8badf377672142bf120681670c0ec8ce6
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-15 14:19:42 +0200

    Update CartProvider.js by adding conditions if the item is already exist so add it to same id if not add as new item.

--- src/components/cart/CartProvider.js
+++ src/components/cart/CartProvider.js
@@ -8,8 +8,22 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === "Add") {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        const existCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existCartItem = state.items[existCartItemIndex];
+        let updatedItems;
+
+        if (existCartItem) {
+            const updateItem = {
+                ...existCartItem,
+                amount: existCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existCartItemIndex] = updateItem;
+        }
+
+        else updatedItems = state.items.concat(action.item);
+
         return {
           items: updatedItems,
           totalAmount: updatedTotalAmount
